30b1aa35d390d60c62fd301c9a3f5823110c682b
LauraWardwell <lwardwell@ou.edu>
2025-02-02 14:42:24 -0600

    Adding mode function to the package

---
+++ R/mode.R
@@ -0,0 +1,12 @@
+#' Mode
+#'
+#' @param v A set of data for which the mode should be calculated
+#'
+#' @returns Mode of the data
+#' @export
+#'
+#' @examples
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
