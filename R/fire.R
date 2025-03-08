66651ef98af646e9a648d5dd1e9a4ba17b9d10c6
LauraWardwell <lwardwell@ou.edu>
2025-03-07 20:44:25 -0600

    Added the FIREDAM dataset for Lab 7

---
+++ R/fire.R
@@ -0,0 +1,14 @@
+#' Fire Damage
+#'
+#' A dataset containing the values for fire damage at accident locations.
+#'
+#' @format
+#' A data frame with 15 rows and 2 columns:
+#' \describe{
+#'   \item{DISTANCE}{Distance from the fire station in miles}
+#'   \item{DAMAGE}{Dollar cost of damage in thousands}
+
+#'   ...
+#' }
+#' @source <https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sinich/p/book/9781498728850>
+"fire"
